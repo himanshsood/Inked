5dc09757842b26bf7cf1c60782d53f65b403b290
Harshita <harshitabansal3142@gmail.com>
2025-03-09 00:14:24 +0530

    Updated MySQL config and fixed environment variable issue

--- api/db.js
+++ api/db.js
@@ -1,10 +1,13 @@
 import mysql from "mysql2";  // Use mysql2 instead of mysql
+import dotenv from "dotenv";
+dotenv.config();
 
 export const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "inkedproject",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect((err) => {
